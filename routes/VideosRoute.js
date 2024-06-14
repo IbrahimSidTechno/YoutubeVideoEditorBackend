9bce91e1fc3e8a12233ecce933047cdbc815b489
ibrahim <ibrahimsidtechno@gmail.com>
2024-06-14 14:18:22 +0500

    cloudinary done

--- routes/VideosRoute.js
+++ routes/VideosRoute.js
@@ -3,6 +3,7 @@ import ytdl from "ytdl-core";
 import { exec } from "child_process";
 import multer from "multer";
 import ffmpeg from 'fluent-ffmpeg'
+import uploadResult from "../utils/cloudinary.js";
 
 
 const route = express.Router();
@@ -20,68 +21,71 @@ const storage = multer.diskStorage({
 const upload = multer({ storage: storage });
 // POST route to handle creating new download statistics
 route.get('/videoget', async (req, res) => {
-    try {
-        // Extract the 'url' parameter from the request query
-        const { url } = req.query;
-        
-        // Log the URL to verify it's received correctly
-        console.log("Received URL:", url);
+  try {
+    // Extract the 'url' parameter from the request query
+    const { url } = req.query;
 
-        // Get video information using ytdl library
-        const info = await ytdl.getInfo(url);
-        
-        // Choose the highest quality format for the video
-        const format = ytdl.chooseFormat(info.formats, { quality: "highest" });
-        
-        // Extract video title from the video information
-        const title = info.videoDetails.title;
-    
-        // Set response headers to indicate file download
-        res.setHeader(
-          "Content-Disposition",
-          `attachment; filename*=UTF-8''${encodeURIComponent(title)}.mp4`
-        );
-        
-        // Log the title to verify it's correctly set
-        console.log("Video Title:", title);
-        
-        // Pipe the video stream from ytdl to the response stream
-        ytdl(url, { format }).pipe(res);
-    } catch (error) {
-        // Handle errors gracefully
-        console.error("Error:", error);
-        res.status(500).send(error.message);
-    }
+    // Log the URL to verify it's received correctly
+    console.log("Received URL:", url);
+
+    // Get video information using ytdl library
+    const info = await ytdl.getInfo(url);
+
+    // Choose the highest quality format for the video
+    const format = ytdl.chooseFormat(info.formats, { quality: "highest" });
+
+    // Extract video title from the video information
+    const title = info.videoDetails.title;
+
+    // Set response headers to indicate file download
+    res.setHeader(
+      "Content-Disposition",
+      `attachment; filename*=UTF-8''${encodeURIComponent(title)}.mp4`
+    );
+
+    // Log the title to verify it's correctly set
+    console.log("Video Title:", title);
+
+    // Pipe the video stream from ytdl to the response stream
+    ytdl(url, { format }).pipe(res);
+  } catch (error) {
+    // Handle errors gracefully
+    console.error("Error:", error);
+    res.status(500).send(error.message);
+  }
 })
 
 route.post("/download/trim", upload.single("video"), async (req, res) => {
   const { startTime, endTime } = req.body;
   const videoPath = req.file.path;
 
+  const re = await uploadResult(videoPath)
+
+  console.log(re.url);
   // Output file path for trimmed video
   const trimmedFilePath = './public/uploads/trimmed_video.mp4';
 
   // Trim the video using ffmpeg
   ffmpeg(videoPath)
-  .setStartTime(startTime)
-  .setDuration(endTime - startTime)
-  .output(trimmedFilePath)
-  .on('end', function() {
-    console.log('Trimmed video saved at:', trimmedFilePath);
-    res.download(trimmedFilePath, 'trimmed_video.mp4', (err) => {
-      if (err) {
-        console.error(`Error sending trimmed video: ${err}`);
-        return res.status(500).json({ error: 'Error sending trimmed video.' });
-      }
-      console.log('Trimmed video sent successfully.');
-      // Uncomment the line below if you want to delete the trimmed video file after sending
-      // fs.unlinkSync(trimmedFilePath);
-    });
-  })
-  .on('error', function(err) {
-    console.error('Error trimming video:', err);
-    return res.status(500).json({ error: 'Error trimming video.' });
-  })
-  .run();
+    .setStartTime(startTime)
+    .setDuration(endTime - startTime)
+    .output(trimmedFilePath)
+    .on('end', function () {
+      console.log('Trimmed video saved at:', trimmedFilePath);
+      res.download(trimmedFilePath, 'trimmed_video.mp4', (err) => {
+        if (err) {
+          console.error(`Error sending trimmed video: ${err}`);
+          return res.status(500).json({ error: 'Error sending trimmed video.' });
+        }
+        console.log('Trimmed video sent successfully.');
+        // Uncomment the line below if you want to delete the trimmed video file after sending
+        // fs.unlinkSync(trimmedFilePath);
+      });
+    })
+    .on('error', function (err) {
+      console.error('Error trimming video:', err);
+      return res.status(500).json({ error: 'Error trimming video.' });
+    })
+    .run();
 });
 export default route;
