7426e50744457c77a3e7e0162ec842b4b4688129
Irtiza Ahmed <hamidsidtechno@gmail.com>
2024-06-20 18:04:39 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,40 +1,61 @@
 import express from 'express';
-// require("dotenv").config();
-import cors from 'cors'
-// import {sendResponse} from './helper/helper.js'
+import cors from 'cors';
 import mongoose from 'mongoose';
-import userRouter from './routes/VideosRoute.js'
+import userRouter from './routes/VideosRoute.js';
+
 const app = express();
 app.use(express.json());
 app.use(cors());
 app.use("/uploads/", express.static("uploads"));
+// app.use("/api", userRouter);
+app.get('/api/videoget', async (req, res) => {
+  try {
+    const { url } = req.query;
+    if (!url) {
+      throw new ApiError(400, 'URL parameter is missing');
+    }
 
-app.use(
-  "/api",
-  userRouter
-);
+    // Get video information using ytdl library
+    const info = await ytdl.getInfo(url);
+    if (!info) {
+      throw new ApiError(404, 'Video information not found');
+    }
 
-const PORT = 4000;
-const MONGODB_URI =
-  "mongodb+srv://ibrahimsidtechno:ibrahim123@cluster0.e5wtkps.mongodb.net/";
-  
+    // Choose the format with both video and audio (highest quality)
+    const format = ytdl.chooseFormat(info.formats, { filter: 'videoandaudio', quality: 'highestvideo' });
+
+    // Set response headers to indicate file download
+    res.setHeader(
+      'Content-Disposition',
+      `attachment; filename*=UTF-8''${encodeURIComponent(info.videoDetails.title)}.mp4`
+    );
+
+    // Pipe the video stream from ytdl to the response stream
+    ytdl(url, { format }).pipe(res);
+  } catch (error) {
+    // Handle errors gracefully
+    console.error('Error:', error);
+    res.status(error.statusCode || 500).json({ message: error.message });
+  }
+});
+
+
+const PORT = process.env.PORT || 4000;
+const MONGODB_URI = "mongodb+srv://ibrahimsidtechno:ibrahim123@cluster0.e5wtkps.mongodb.net/";
+
 const connectWithRetry = () => {
-  mongoose
-    .connect(MONGODB_URI)
+  mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => {
-      console.log(
-        `Database Is Successfuly Connect And Running On => ${PORT} Port`
-      );
-      app.listen(process.env.PORT);
+      console.log(`Database connected successfully.`);
     })
     .catch((err) => {
-      console.log("Database connection unsuccessful, retry after 5 seconds.");
-      console.error(err);
+      console.error("Database connection unsuccessful:", err.message);
       setTimeout(connectWithRetry, 5000);
     });
 };
+
 connectWithRetry();
 
 const server = app.listen(PORT, () => {
-  console.log(`Server successFully Running on ${PORT}`);
+  console.log(`Server successfully running on port ${PORT}`);
 });
