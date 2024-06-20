5a47c119db45f090f368b7692853e6b5f147a05f
Irtiza Ahmed <hamidsidtechno@gmail.com>
2024-06-20 16:48:55 +0500

    Update VideosRoute.js

--- routes/VideosRoute.js
+++ routes/VideosRoute.js
@@ -6,7 +6,7 @@ import upload from "../midlewares.js/multer.js";
 const route = express.Router();
 
 
-route.route("/videosend").post(upload.single('video'),videosend)
+route.route("/videosend").post(videosend)
 route.route("/videoget/:id").get(videoGetById)
 route.route("/videoget").get(videoGet)
 route.route("/download/trim").post(downloadTrim)
