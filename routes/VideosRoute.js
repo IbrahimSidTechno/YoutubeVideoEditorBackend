101f85abaf39639b3e2e79fb4b1511ca39a7cc85
Irtiza Ahmed <hamidsidtechno@gmail.com>
2024-06-21 16:21:43 +0500

    upload

--- routes/VideosRoute.js
+++ routes/VideosRoute.js
@@ -1,11 +1,11 @@
 import express from "express";
-import { downloadTrim, videoGet, videoGetById, videosend } from "../controllers/videoControllers.js";
+import { downloadTrim, getAllStatus, videoGet, videoGetById, videosend } from "../controllers/videoControllers.js";
 import upload from "../midlewares.js/multer.js";
 
 
 const route = express.Router();
 
-
+route.route("/getallstatus").get(getAllStatus)
 route.route("/videosend").post(videosend)
 route.route("/videoget/:id").get(videoGetById)
 route.route("/videoget").get(videoGet)
